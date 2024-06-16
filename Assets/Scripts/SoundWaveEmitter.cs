51d913db6d0f2503b6fe55465eaf6dde5746057b
agalloch21 <agalloch21@gmail.com>
2024-06-16 18:22:23 +0800

    finished rewriting soundwave emitter

--- Assets/Scripts/SoundWaveEmitter.cs
+++ Assets/Scripts/SoundWaveEmitter.cs
@@ -1,14 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.VFX;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 using HoloKit;
-using UnityEngine.InputSystem;
-using UnityEngine.XR.ARFoundation.Samples;
 
 
 public class SoundWave
@@ -42,66 +35,39 @@ public class SoundWave
 
 public class SoundWaveEmitter : MonoBehaviour
 {
-    
+    [Header("References")]
     [SerializeField] HoloKitCameraManager cameraManager;
     [SerializeField] HolokitAudioProcessor audioProcessor;
     [SerializeField] DepthImageProcessor depthImageProcessor;
     [SerializeField] TrackedPoseDriver trackedPoseDriver;
 
-    [Header("References")]
-    public VisualEffect vfx;
-    public Material matMeshing;
+    [Header("Effect")]
+    [SerializeField] VisualEffect vfx;
+    [SerializeField] Material matMeshing;
 
     [Header("SoundWave Settings")]
-    [Tooltip("Fade In/Out Duration")]
-    public Vector2 soundwaveLife = new Vector2(4, 6);
+    [SerializeField] Vector2 soundwaveLife = new Vector2(0.5f, 1.5f);
+    [SerializeField] Vector2 soundwaveSpeed = new Vector2(3f, 4.5f);
+    [SerializeField] Vector2 soundwaveAngle = new Vector2(90, 180);
+    [SerializeField] float minWaveThickness = 1;
+    [SerializeField] float emitVolumeThreshold = 0.02f;
 
-    [Tooltip("March Speed")]
-    public Vector2 soundwaveSpeed = new Vector2(1, 4);
-
-    //[Tooltip("Strength")]
-    //public Vector2 soundwaveStrength = new Vector2(0, 1);
-
-    [Tooltip("Angle")]
-    public Vector2 soundwaveAngle = new Vector2(90, 180);
-
-    public float minWaveThickness = 1;
-    public float emitVolumeThreshold = 0.02f;
-    //public float particleSweepThickness = 0.36f;
-    float maxWaveRange = 50;
 
     const int MAX_SOUND_WAVE_COUNT = 3;
     SoundWave[] soundwaves = new SoundWave[MAX_SOUND_WAVE_COUNT];
-
     int nextEmitIndex = 0;
+    float maxWaveRange = 50;
+    float smoothedSoundVolume = 0;
+    float smoothedSoundPitch = 0;
+    bool debugMode = false;
+
     float[] rippleOriginList;
     float[] rippleDirectionList;
-    //float[] rippleAliveList;
     float[] rippleAgeList;
     float[] rippleRangeList;
     float[] rippleAngleList;
     float[] rippleThicknessList;
-    float lastSoundVolume = 0;
-    float lastSoundPitch = 0;
 
-    [Header("Debug")]
-    public bool debugMode = false;
-    [Range(0.0f, 1.0f)]
-    public float testAge = 0;
-    [Range(0.0f, 10.0f)]
-    public float testRange = 1;
-    [Range(0.0f, 180.0f)]
-    public float testAngle = 90;
-    [Range(0.01f, 5.0f)]
-    public float testThickness = 2f;
-
-    [Range(0f, 1f)]
-    public float testAudioVolume = 0f;
-
-    [Range(0f, 1f)]
-    public float testAudioPitch = 0f;
-
-        
 
     void Start()
     {
@@ -115,7 +81,6 @@ public class SoundWaveEmitter : MonoBehaviour
         // Init Mat Related Parameters
         rippleOriginList = new float[MAX_SOUND_WAVE_COUNT * 3];
         rippleDirectionList = new float[MAX_SOUND_WAVE_COUNT * 3];
-        //rippleAliveList = new float[MAX_SOUND_WAVE_COUNT];
         rippleAgeList = new float[MAX_SOUND_WAVE_COUNT];
         rippleRangeList = new float[MAX_SOUND_WAVE_COUNT];
         rippleAngleList = new float[MAX_SOUND_WAVE_COUNT];
@@ -124,6 +89,11 @@ public class SoundWaveEmitter : MonoBehaviour
 
     void Update()
     {
+        // Smoothed Sound
+        float temp_vel = 0;
+        smoothedSoundVolume = Mathf.SmoothDamp(smoothedSoundVolume, audioProcessor.AudioVolume, ref temp_vel, 0.05f);
+        smoothedSoundPitch = Mathf.SmoothDamp(smoothedSoundPitch, audioProcessor.AudioPitch, ref temp_vel, 0.05f);
+
         // Emit New SoundWave
         if (Input.GetMouseButton(0) || audioProcessor.AudioVolume > emitVolumeThreshold)
         {
@@ -134,63 +104,14 @@ public class SoundWaveEmitter : MonoBehaviour
             StopAllSoundWaves();
         }
 
-
         // Update Extant SoundWave        
         UpdateSoundWave();
 
-
-        //// If in Debug Mode, rewrite data
-        //if (debugMode)
-        //{
-        //    for (int i = 0; i < soundwaves.Length; i++)
-        //    {
-        //        SoundWave wave = soundwaves[i];
-        //        // Activate Wave0
-        //        if (i == 0)
-        //        {
-        //            wave.origin = head_transform.position;
-        //            wave.direction = Quaternion.Euler(head_transform.eulerAngles) * Vector3.forward;
-
-        //            wave.alive = 1;
-        //            wave.age_in_percentage = testAge;
-        //            wave.range = testRange;
-        //            wave.angle = testAngle;
-        //            wave.thickness = testThickness;
-
-        //            PushInitialChanges(i);
-        //        }
-
-        //        // Deactivate others
-        //        else
-        //        {
-        //            DeactivateSoundWave(i);
-        //        }
-        //    }
-        //}
-        
-
         // Push changes to VFX and Mat
         PushIteratedChanges();
-
-
-
-        
-
-        if (depthImageProcessor != null)
-        {
-            Texture2D human_tex = depthImageProcessor.HumanStencilTexture;
-            if(human_tex != null)
-            {
-                human_tex.wrapMode = TextureWrapMode.Repeat;
-                vfx.SetTexture("HumanStencilTexture", human_tex);
-                vfx.SetMatrix4x4("HumanStencilTextureMatrix", depthImageProcessor.DisplayRotatioMatrix);
-                matMeshing.SetTexture("_HumanStencilTexture", human_tex);
-                matMeshing.SetMatrix("_HumanStencilTextureMatrix", depthImageProcessor.DisplayRotatioMatrix);
-            }
-        }
-
     }
 
+    #region Emit/Stop/Update SoundWave
     void EmitSoundWave()
     {        
         int cur_emit_index = GetCurrentWaveIndex();
@@ -210,19 +131,13 @@ public class SoundWaveEmitter : MonoBehaviour
         new_wave.origin = pos;
         new_wave.direction = dir;
 
-        float volume = audioProcessor.AudioVolume;
-        float pitch = audioProcessor.AudioPitch;
         new_wave.speed = Random.Range(soundwaveSpeed.x, soundwaveSpeed.y);// * pitch; 
-        new_wave.life = Random.Range(soundwaveLife.x, soundwaveLife.y) * Utilities.Remap(pitch, 0f, 1f, 1f, 1.5f); // relative to pitch
-        //new_wave.strength = Random.Range(soundwaveStrength.x, soundwaveStrength.y) * volume; // relative to volume
-        new_wave.angle = Utilities.Remap(volume, 0, 1, soundwaveAngle.x, soundwaveAngle.y); // relative to volume
-
+        new_wave.life = Random.Range(soundwaveLife.x, soundwaveLife.y) * Utilities.Remap(smoothedSoundPitch, 0f, 1f, 1f, 1.5f); // relative to pitch
+        new_wave.angle = Utilities.Remap(smoothedSoundVolume, 0, 1, soundwaveAngle.x, soundwaveAngle.y); // relative to volume
 
         ActivateSoundWave(next_emit_index);
 
         PushInitialChanges(next_emit_index);
-
-        Debug.Log("Emit new SoundWave, Index:" + next_emit_index);
 
         MoveWaveIndex();        
     }
@@ -285,6 +200,79 @@ public class SoundWaveEmitter : MonoBehaviour
         }
     }
 
+    void PushInitialChanges(int index)
+    {
+        // VFX
+        SoundWave wave = soundwaves[index];
+        vfx.SetVector3("WaveOrigin", wave.origin);
+        vfx.SetVector3("WaveDirection", wave.direction);
+        vfx.SetFloat("WaveAge", wave.age_in_percentage);
+        vfx.SetFloat("WaveRange", wave.range);
+        vfx.SetFloat("WaveAngle", wave.angle);
+
+
+        // Material
+        rippleOriginList[index * 3] = wave.origin.x; rippleOriginList[index * 3 + 1] = wave.origin.y; rippleOriginList[index * 3 + 2] = wave.origin.z;
+        rippleDirectionList[index * 3] = wave.direction.x; rippleDirectionList[index * 3 + 1] = wave.direction.y; rippleDirectionList[index * 3 + 2] = wave.direction.z;
+
+        rippleAgeList[index] = wave.age_in_percentage;
+        rippleRangeList[index] = wave.range;
+        rippleAngleList[index] = wave.angle;
+        rippleThicknessList[index] = wave.thickness;
+
+        matMeshing.SetFloatArray("rippleOriginList", rippleOriginList);
+        matMeshing.SetFloatArray("rippleDirectionList", rippleDirectionList);
+        matMeshing.SetFloatArray("rippleAgeList", rippleAgeList);
+        matMeshing.SetFloatArray("rippleRangeList", rippleRangeList);
+        matMeshing.SetFloatArray("rippleAngleList", rippleAngleList);
+        matMeshing.SetFloatArray("rippleThicknessList", rippleThicknessList);
+
+        matMeshing.SetFloat("_WaveMinThickness", minWaveThickness);
+        matMeshing.SetVector("_WaveOrigin", wave.origin);
+    }
+
+    void PushIteratedChanges()
+    {
+
+        // VFX, Update using latest sound wave
+        int cur_wave_index = GetCurrentWaveIndex();
+        SoundWave wave = soundwaves[cur_wave_index];
+
+        vfx.SetFloat("WaveRange", wave.range);
+        vfx.SetFloat("WaveAge", wave.age_in_percentage);
+
+        // VFX, Update Depth Image
+        if (depthImageProcessor != null)
+        {
+            Texture2D human_tex = depthImageProcessor.HumanStencilTexture;
+            if (human_tex != null)
+            {
+                human_tex.wrapMode = TextureWrapMode.Repeat;
+                vfx.SetTexture("HumanStencilTexture", human_tex);
+                vfx.SetMatrix4x4("HumanStencilTextureMatrix", depthImageProcessor.DisplayRotatioMatrix);
+                matMeshing.SetTexture("_HumanStencilTexture", human_tex);
+                matMeshing.SetMatrix("_HumanStencilTextureMatrix", depthImageProcessor.DisplayRotatioMatrix);
+            }
+        }
+
+        // Material
+        for (int i = 0; i < MAX_SOUND_WAVE_COUNT; i++)
+        {
+            rippleAgeList[i] = soundwaves[i].age_in_percentage;
+            rippleRangeList[i] = soundwaves[i].range;
+            rippleThicknessList[i] = soundwaves[i].thickness;
+        }
+        matMeshing.SetFloatArray("rippleAgeList", rippleAgeList);
+        matMeshing.SetFloatArray("rippleRangeList", rippleRangeList);
+        matMeshing.SetFloatArray("rippleThicknessList", rippleThicknessList);
+
+        matMeshing.SetFloat("_SoundVolume", smoothedSoundVolume);
+        matMeshing.SetFloat("_SoundPitch", smoothedSoundPitch);
+    }
+    #endregion
+
+
+    #region Other Functions
     bool IsWaveTotallyDead(SoundWave wave)
     {
         return wave.alive == 0 && wave.age == 0 && wave.thickness >= minWaveThickness && wave.range >= maxWaveRange/* to make it die far*/;
@@ -317,92 +305,16 @@ public class SoundWaveEmitter : MonoBehaviour
 
         return (nextEmitIndex == 0 ? MAX_SOUND_WAVE_COUNT - 1 : nextEmitIndex - 1);
     }
+
     int GetNextWaveIndex()
     {
         return nextEmitIndex;
     }
+
     int MoveWaveIndex()
     {
         nextEmitIndex = (nextEmitIndex == MAX_SOUND_WAVE_COUNT - 1 ? 0 : nextEmitIndex + 1);
         return nextEmitIndex;
-    }
-
-    
-
-    void PushInitialChanges(int index)
-    {
-        // VFX
-        SoundWave wave = soundwaves[index];
-        vfx.SetVector3("WaveOrigin", wave.origin);
-        vfx.SetVector3("WaveDirection", wave.direction);
-        vfx.SetFloat("WaveAge", wave.age_in_percentage);
-        vfx.SetFloat("WaveRange", wave.range);
-        vfx.SetFloat("WaveAngle", wave.angle);
-        //vfx.SetFloat("WaveThickness", particleSweepThickness);
-
-
-        // Material
-        rippleOriginList[index * 3] = wave.origin.x; rippleOriginList[index * 3 + 1] = wave.origin.y; rippleOriginList[index * 3 + 2] = wave.origin.z;
-        rippleDirectionList[index * 3] = wave.direction.x; rippleDirectionList[index * 3 + 1] = wave.direction.y; rippleDirectionList[index * 3 + 2] = wave.direction.z;
-
-        //rippleAliveList[index] = wave.alive;
-        rippleAgeList[index] = wave.age_in_percentage;
-        rippleRangeList[index] = wave.range;
-        rippleAngleList[index] = wave.angle;
-        rippleThicknessList[index] = wave.thickness;
-
-        matMeshing.SetFloatArray("rippleOriginList", rippleOriginList);
-        matMeshing.SetFloatArray("rippleDirectionList", rippleDirectionList);
-        matMeshing.SetFloatArray("rippleAgeList", rippleAgeList);
-        matMeshing.SetFloatArray("rippleRangeList", rippleRangeList);
-        matMeshing.SetFloatArray("rippleAngleList", rippleAngleList);
-        matMeshing.SetFloatArray("rippleThicknessList", rippleThicknessList);
-
-        // set separately 
-        matMeshing.SetVector("_WaveOrigin", wave.origin);
-        //matMeshing.SetFloat("_WaveSpeed", wave.speed);
-
-    }
-
-    void PushIteratedChanges()
-    {
-
-        // VFX, Updated by the latest sound wave
-        int cur_wave_index = GetCurrentWaveIndex();
-        SoundWave wave = soundwaves[cur_wave_index];
-
-        vfx.SetFloat("WaveRange", wave.range);
-        vfx.SetFloat("WaveAge", wave.age_in_percentage);
-        //vfx.SetFloat("WaveThickness", wave.thickness);
-
-
-
-        // Material
-        for (int i = 0; i < MAX_SOUND_WAVE_COUNT; i++)
-        {
-            //rippleAliveList[i] = soundwaves[i].alive;
-            rippleAgeList[i] = soundwaves[i].age_in_percentage;
-            rippleRangeList[i] = soundwaves[i].range;
-            rippleThicknessList[i] = soundwaves[i].thickness;
-
-            HelperModule.Instance.SetInfo($"Wave{i}_Range", rippleRangeList[i].ToString());
-            HelperModule.Instance.SetInfo($"Wave{i}_Thickness", rippleThicknessList[i].ToString());
-        }
-        //matMeshing.SetFloatArray("rippleAliveList", rippleAliveList);
-        matMeshing.SetFloatArray("rippleAgeList", rippleAgeList);
-        matMeshing.SetFloatArray("rippleRangeList", rippleRangeList);
-        matMeshing.SetFloatArray("rippleThicknessList", rippleThicknessList);
-
-        float src_value = lastSoundVolume;
-        float dst_value = debugMode ? testAudioVolume : audioProcessor.AudioVolume;
-        float temp_vel = 0;
-        matMeshing.SetFloat("_SoundVolume", Mathf.SmoothDamp(src_value, dst_value, ref temp_vel, 0.05f));
-
-        src_value = lastSoundPitch;
-        dst_value = debugMode ? testAudioPitch : audioProcessor.AudioPitch;
-        temp_vel = 0;
-        matMeshing.SetFloat("_SoundPitch", Mathf.SmoothDamp(src_value, dst_value, ref temp_vel, 0.05f));
-
     }
 
     float[] Vector3ToArray(Vector3 vec)
@@ -420,4 +332,5 @@ public class SoundWaveEmitter : MonoBehaviour
         Debug.Log(prefix + string.Format("|{0}, alive:{1}, age:{2}, range:{3}, angle:{4}, thickness:{5}, origin:{6}, dir:{7}",
                 index, wave.alive, wave.age, wave.range, wave.angle, wave.thickness, wave.origin, wave.direction));
     }
+    #endregion
 }
